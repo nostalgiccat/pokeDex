66cbc96b43b276f881811b1d6f9b4c247f03923e
nostalgiccat <nesdom27@gmail.com>
2023-10-07 15:33:35 -0700

    Redesigned layout using pokedex background image and positioned 1 pokemon card to display on the screen, added next button

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let offset = 0;
 let pageNumber = 1;
 
 function fetchPokemon() {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=1&offset=${offset}`)
         .then(response => response.json())
         .then(data => {
             const pokemonArray = data.results;
@@ -23,9 +23,13 @@ function fetchPokemon() {
                         const pokemonCard = document.createElement('div');
                         pokemonCard.className = 'pokemon-card';
                         pokemonCard.innerHTML = `
-                            <h3>${pokeData.id}</h3>
-                            <h3>${pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)}</h3>
+                            ${pokeData.id}
+                            </br>
+                            ${pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)}
+                            </br>
+                            </br>
                             <img src="${pokeData.sprites.other.dream_world.front_default}" alt="${pokeData.name}">
+                            <!--
                             <p>HP: ${pokeData.stats[0].base_stat}</p>
                             <p>Attack: ${pokeData.stats[1].base_stat}</p>
                             <p>Defense: ${pokeData.stats[2].base_stat}</p>
@@ -41,26 +45,26 @@ function fetchPokemon() {
 
 
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("page-number").innerText = `Page ${pageNumber}`;
+    // document.getElementById("page-number").innerText = `Page ${pageNumber}`;
     fetchPokemon();
 });
 
 
-document.getElementById("next").addEventListener("click", function(){
+document.getElementById("next-button").addEventListener("click", function(){
     console.log("next");
-    offset = offset + 20;
+    offset = offset + 1;
     pageNumber++; // Increment the page number 
-    document.getElementById("page-number").innerText = `Page ${pageNumber}`
+    // document.getElementById("page-number").innerText = `Page ${pageNumber}`
     fetchPokemon()
 
 })
 
 document.getElementById("previous").addEventListener("click", function(){
     console.log("previous");
-    if (offset >= 20) {
-        offset = offset-20;
+    if (offset > 1) {
+        offset = offset-1;
         pageNumber--; // Decrease page number
-        document.getElementById("page-number").innerText = `Page ${pageNumber}`
+        // document.getElementById("page-number").innerText = `Page ${pageNumber}`
         fetchPokemon();
     }
 })
